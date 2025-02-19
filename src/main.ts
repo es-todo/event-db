6ccff90c157203cc8aa52188110d27e55d14c215
Abdulaziz Ghuloum <aghuloum@gmail.com>
2025-02-19 05:32:39 +0300

    added pg module and testing connection pooling

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import pg from "pg";
+
+const port = 3000;
+const db_user = "admin";
+const db_pass = "letmein";
+const db_name = "eventdb";
+
+const pool = new pg.Pool({
+  user: db_user,
+  password: db_pass,
+  database: db_name,
+});
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (_req, res) => {
+  try {
+    const pgres = await pool.query("SELECT NOW() as t");
+    res.send(`Hello World! Time is ${pgres.rows[0].t}`);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 app.listen(port, () => {
